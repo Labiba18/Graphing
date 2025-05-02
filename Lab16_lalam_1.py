ebd28db0c89a6b652663e02ace13a9f35b720b9b
Labiba18 <lalam@student.cscc.edu>
2025-05-02 13:25:02 -0400

    P2-Read CSV and printed header with enumerate()

--- Lab16_lalam_1.py
+++ Lab16_lalam_1.py
@@ -5,3 +5,15 @@ Description: Reads and plots U.S. unemployment data from FRED (UNRATE).
 Date: 2025-05-02
 """
 
+import csv
+from datetime import datetime
+import matplotlib.pyplot as plt
+
+filename = 'unemployment.csv'
+
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
