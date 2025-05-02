9736400af43c0188b59d69fe8f3efc8fcf817964
Labiba18 <lalam@student.cscc.edu>
2025-05-02 12:52:45 -0400

    P1- Added basic sin(x)/x plot

--- Lab13_lalam_1.py
+++ Lab13_lalam_1.py
@@ -4,3 +4,14 @@ Author: Labiba Alam
 Description: Plotting sin(x)/x for Lab 13 graph assignment
 Date: 4/30/25
 """
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create x and y values
+x = np.linspace(-20, 20, 1000)
+y = np.sinc(x / np.pi)  # sinc(x) = sin(x)/x
+
+# Plot the graph
+plt.plot(x, y)
+plt.show()
